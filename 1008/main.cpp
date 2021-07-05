f618a4f6bb5d1606e9756bd58d569a8c92df758c
dkssyd07 <line102784@gmail.com>
2021-07-05 16:04:40 -0600

    Initial Commit

---
+++ 1008/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1008
+//
+//  Created by Dongho Kim on 2021-07-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
